db7fef74d555152c39fc47a551353499dcd33c12
maia-g <maia.griffith@duke.edu>
2024-09-27 10:06:59 -0400

    Filter records going into dictionaries

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -41,9 +41,11 @@ for lineString in line_list:
     obs_lat = lineData[6]
     obs_lon = lineData[7]
 
-    #Add items to dictionaries
-    date_dict[record_id] = obs_date # using record_id as key, obs_date as value
-    location_dict[record_id] = (obs_lat, obs_lon) # using record_id as key, and (lat,lon) as value
+    # Determine if location class criteria is met
+    if obs_lc in ("1","2","3"):
+        #Add items to dictionaries if lc is met
+        date_dict[record_id] = obs_date # using record_id as key, obs_date as value
+        location_dict[record_id] = (obs_lat, obs_lon) # using record_id as key, and (lat,lon) as value
 
     #Print the location of sara
     #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
